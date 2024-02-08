d8fda6e2877a4afc98bbe881b04b3bae753af642
Klaudia Góralska <klaudia.goralska95@gmail.com>
2024-02-08 18:40:53 +0100

    check in

--- src/Components/ChildrenList/ChildrenList.jsx
+++ src/Components/ChildrenList/ChildrenList.jsx
@@ -1,28 +1,62 @@
 import {useChildren} from "../../Querries/useChildren";
-import {Avatar, Box, List, ListItemAvatar, ListItemButton, ListItemText, Typography} from "@mui/material";
+import {
+    Avatar,
+    Button,
+    List,
+    ListItemAvatar,
+    ListItemButton,
+    ListItemText,
+    Paper,
+    Typography
+} from "@mui/material";
 import dayjs from "dayjs";
 import {useChildrenMutations} from "../../Querries/useChildrenMutations";
+import {useState} from "react";
+import LoginIcon from '@mui/icons-material/Login';
+import Grid from '@mui/material/Unstable_Grid2'
+import CheckIcon from '@mui/icons-material/Check';
 
 export const ChildrenList = () => {
     const {data} = useChildren();
+    const [showCheckIn, setShowCheckIn] = useState(null)
     const {checkIn, checkOut} = useChildrenMutations();
+    console.log('showCheckIn', showCheckIn)
+    console.log('data', data)
+
+    const checkInYourChild = (id) => {
+        checkIn.mutate(id, {
+            onSuccess: ()=> setShowCheckIn(null)
+        })
+    }
 
     return (
-        <Box sx={{width: '100%', maxWidth: 500}}> <Typography align={'left'} variant="h4" gutterBottom>
+        <Paper sx={{width: '100%', maxWidth: 500}}>
+            <Typography align={'left'} variant="h4" gutterBottom>
             Children of Famly
         </Typography>
             <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
                 {data?.children?.map((child) =>
-                    <ListItemButton>
+            <Grid container spacing={2}>
+                <Grid item xs={10} style={{flexDirection: 'row'}}>
+                    <ListItemButton key={child.childId} onClick={()=>setShowCheckIn(child.childId)}>
                         <ListItemAvatar>
                             <Avatar alt={child.name.fullName} src={child.image?.small ?? ''}/>
                         </ListItemAvatar>
                         <ListItemText primary={child.name.fullName}
                                       secondary={child.birthday ? dayjs(child.birthday).format('MMM DD, YYYY') : ''}/>
-                    </ListItemButton>)
+                        {child.checkedIn && <CheckIcon color="success" />}
+                    </ListItemButton>
 
-                }
+                </Grid>
+                {showCheckIn === child.childId &&
+                <Grid item xs={2} style={{alignSelf: 'center'}}>
+                    <Button variant="contained" onClick={()=>checkInYourChild(child.childId)} color="success" style={{width: '150px', margin: 0}} endIcon={<LoginIcon/>}>
+                        Check in
+                    </Button>
+                </Grid>}
+            </Grid>
+                )}
             </List>
-        </Box>
+        </Paper>
     )
 }
--- src/Querries/useChildrenMutations.jsx
+++ src/Querries/useChildrenMutations.jsx
@@ -1,18 +1,23 @@
-import {useMutation} from "@tanstack/react-query";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import dayjs from "dayjs";
 
 export const useChildrenMutations = () => {
+    const queryClient = useQueryClient()
+
     const checkIn = useMutation({ mutationFn: async (childId)=>{
-            const response = await fetch(`https://app.famly.co/api/v2/children/${childId}/checkins` + new URLSearchParams({
+            const response = await fetch(`https://app.famly.co/api/v2/children/${childId}/checkins?` + new URLSearchParams({
                 accessToken: '1127a03c-ed76-41d5-9058-f9ca105c41cf',
-                pickupTime: '16:00'
+                pickupTime: dayjs().add(2, 'hours').format('HH:mm')
             }) ,{
                 method: "POST"
             });
             return await response.json();
-        } })
+        },
+    onSuccess: () => queryClient.invalidateQueries({queryKey: ['childrenList']})
+    })
 
     const checkOut = useMutation({ mutationFn: async (childId)=>{
-            const response = await fetch(`https://app.famly.co/api/v2/children/${childId}/checkout` + new URLSearchParams({
+            const response = await fetch(`https://app.famly.co/api/v2/children/${childId}/checkout?` + new URLSearchParams({
                 accessToken: '1127a03c-ed76-41d5-9058-f9ca105c41cf',
             }) ,{
                 method: "POST"
